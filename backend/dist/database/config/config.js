695028b4beab1363c02241e5b849bafa4b65dbd2
matheus jessé <matheus_jesse@hotmail.com>
2024-05-14 08:04:43 -0300

    chore: configuração do host do sequelize

--- backend/dist/database/config/config.js
+++ backend/dist/database/config/config.js
@@ -1,23 +1,11 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+"use strict";
+require("dotenv/config");
+const config = {
+    username: process.env.DB_USER || 'postgres',
+    password: process.env.DB_PASS || '123456',
+    database: process.env.DB_NAME || 'matricula-db',
+    host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT || 5432),
+    dialect: 'postgres',
+};
+module.exports = config;
--- backend/src/database/config/config.ts
+++ backend/src/database/config/config.ts
@@ -5,7 +5,7 @@ const config: Options = {
   username: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASS || '123456',
   database: process.env.DB_NAME || 'matricula-db',
-  host: process.env.DB_HOST || 'db',
+  host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT || 5432),
   dialect: 'postgres',
 };
